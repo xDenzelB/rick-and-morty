d31cbb0637b021db3e80e667fcf2e3751a84b0cc
Denzel <dbartolaba123@gmail.com>
2022-02-08 15:53:10 -0800

    Added in RickPage for page buttons

---
+++ src/RickPage.js
@@ -0,0 +1,30 @@
+import { React, useEffect, useState } from 'react';
+import './App.css';
+import { getAllCharacters } from './services/fetch-utils';
+
+
+export default function RickPage() {
+  const [allCharacters, setAllCharacters] = useState([]);
+  const [page, setPage] = useState(1);
+  const perPage = 50;
+
+  useEffect(() => {
+    async function fetch() {
+      const from = page * perPage - perPage;
+      const to = page * perPage;
+      const rickAndMorty = await getAllCharacters(from, to);
+
+      setAllCharacters(rickAndMorty);
+    }
+
+    fetch();
+  }, [page]);
+  return <div>
+    <h2>Current Page {page}</h2>
+
+    <div>
+      <button disabled={page <= 1} onClick={() => setPage(page - 1)}>Previous Page</button>
+      <button onClick={() => setPage(page + 1)}>Previous Page</button>
+    </div>
+  </div>;
+}
