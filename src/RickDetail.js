6b71c11dfef71d51296ee3a2712fe9feaf2604c6
Denzel <dbartolaba123@gmail.com>
2022-02-08 17:40:56 -0800

    added in useeffect for detail page

--- src/RickDetail.js
+++ src/RickDetail.js
@@ -1,5 +1,24 @@
-import React from 'react';
-
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useRouteMatch } from 'react-router-dom/cjs/react-router-dom.min';
+import { getCharacter } from './services/fetch-utils';
 export default function RickDetail() {
+
+  const [character, setCharacter] = useState({});
+  const matching = useRouteMatch();
+
+  useEffect(() => {
+    async function fetch() {
+      const rick = await getCharacter(matching.params.id);
+
+      setCharacter(rick);
+    }
+
+    fetch();
+  }, [matching.params.id]);
+
+  function handleRickAndMortyClick() {
+    window.location.href = character.link;
+  }
   return <div></div>;
 }
